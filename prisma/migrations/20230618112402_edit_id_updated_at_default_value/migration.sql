b0177c9bfc918e8e9ffa0d65c6021d34a4ce5430
De Jong Yeong <dejong.re2@gmail.com>
2023-06-18 12:24:52 +0100

    edit db prisma schema for default value

---
+++ prisma/migrations/20230618112402_edit_id_updated_at_default_value/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE "invites" ALTER COLUMN "id" SET DEFAULT uuid_generate_v4(),
+ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "machine_types" ALTER COLUMN "id" SET DEFAULT uuid_generate_v4(),
+ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "machines" ALTER COLUMN "id" SET DEFAULT uuid_generate_v4(),
+ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "profiles" ALTER COLUMN "id" SET DEFAULT uuid_generate_v4(),
+ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "roles" ALTER COLUMN "id" SET DEFAULT uuid_generate_v4(),
+ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
