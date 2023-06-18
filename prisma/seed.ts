4c720bdbaa2547c1a7bb4fd3ee8a2b71d6f72f9c
De Jong Yeong <dejong.re2@gmail.com>
2023-06-18 17:33:34 +0100

    seed role data

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import { prisma } from '../src/lib/prisma/db';
+
+async function seed() {
+  try {
+    await prisma.role.createMany({
+      data: [
+        { name: 'Admin', descr: 'AM Administrator', alias: 'am-admin' },
+        { name: 'Manager', descr: 'AM Manager', alias: 'am-manager' },
+        {
+          name: 'Production Team',
+          descr: 'AM Production Team',
+          alias: 'am-prod-team',
+        },
+        {
+          name: 'Service Team',
+          descr: 'AM Service Team',
+          alias: 'am-service-team',
+        },
+        { name: 'Dealership', descr: 'Dealership', alias: 'dealership' },
+        { name: 'Farm Manager', descr: 'Farm Manager', alias: 'farm-manager' },
+        { name: 'Farmer', descr: 'Farmer', alias: 'farmer' },
+      ],
+    });
+
+    console.log('Role seeded successfully');
+  } catch (error) {
+    console.error('Failed to create seed data: ', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+seed();
