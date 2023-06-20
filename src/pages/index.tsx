fc1a6edeb140b0572270fb29a49f36125c4d33b7
De Jong Yeong <dejong.re2@gmail.com>
2023-06-20 11:22:59 +0100

    install influxdb data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,3 +42,22 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
     },
   };
 };
+
+// import ErrorFallback from '@/components/error/ErrorFallback';
+// import { useRouter } from 'next/router';
+
+// const CustomErrorComponent = () => {
+//   const router = useRouter();
+
+//   const onRetry = () => {
+//     router.reload();
+//   };
+
+//   const onGoHome = () => {
+//     router.push('/');
+//   };
+
+//   return <ErrorFallback onRetry={onRetry} onGoHome={onGoHome} />;
+// };
+
+// export default CustomErrorComponent;
