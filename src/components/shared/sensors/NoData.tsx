2a6f6389aac91a7f16e93264ab755106cf4dbe9d
De Jong Yeong <dejong.re2@gmail.com>
2023-06-29 20:18:34 +0100

    add css to text

--- src/components/shared/sensors/NoData.tsx
+++ src/components/shared/sensors/NoData.tsx
@@ -3,7 +3,9 @@ import { Empty } from 'antd';
 export default function NoData() {
   const descr = (
     <div className="text-xs mt-3 flex flex-col gap-1">
-      <p className="font-bold">No data available in the selected date range.</p>
+      <p className="font-bold text-red-600">
+        No data available for the selected machine and/or date range.
+      </p>
       <p>
         Note: Data Retention Period on InfluxDB Cloud Trial Plan is 30 days
         only.
