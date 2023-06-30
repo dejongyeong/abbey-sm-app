3f41866b0e9f7f2deb61e0b823f49573637ecc0e
De Jong Yeong <dejong.re2@gmail.com>
2023-06-30 20:42:05 +0100

    move role api to roles folder

--- src/pages/api/roles/index.ts
+++ src/pages/api/roles/index.ts
@@ -12,14 +12,17 @@ export default async function handler(
     if (!session) {
       res
         .status(401)
-        .json({ message: 'No active session or is not authenticated' });
+        .json({
+          roles: null,
+          message: 'No active session or is not authenticated',
+        });
     } else {
       const roles = await countUsersByRoles();
-      res.status(200).json(roles);
+      res.status(200).json({ roles: roles, message: 'Success' });
     }
   } else {
     res.setHeader('Allow', 'GET');
-    res.status(405).json({ message: 'Method not allowed' });
+    res.status(405).json({ roles: null, message: 'Method not allowed' });
   }
 }
 
