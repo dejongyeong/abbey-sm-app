c11ee05256c270d1c1c60c5db920650a2dc6f6a0
De Jong Yeong <dejong.re2@gmail.com>
2023-06-30 22:16:10 +0100

    add data to table from database

--- src/components/user/UserTable.tsx
+++ src/components/user/UserTable.tsx
@@ -5,9 +5,17 @@ const { Text } = Typography;
 
 export default function UserTable({ users }: any) {
   const dataSource = users?.map((user: any) => ({
-    key: user.sb_auth_id,
+    key: user.id,
     first_name: user.first_name,
     last_name: user.last_name,
+    email: user.email,
+    phone: `${user.dial_code} ${user.phone}`,
+    role: user.role.name,
+    inviter: `${user.invites_received[0]?.sender.first_name} ${user.invites_received[0]?.sender.last_name}`,
+    status: user.invites_received[0]?.is_accepted,
+    company: `${user.company.name ? user.company.name : '-'}`,
+    country: `${user.company.state ? user.company.state : '-'}`,
+    zip: `${user.company.zip ? user.company.zip : '-'}`,
   }));
 
   console.log(users);
@@ -21,9 +29,9 @@ export default function UserTable({ users }: any) {
         <Table
           size="middle"
           rowKey="key"
-          bordered={false}
+          bordered={true}
           loading={false}
-          scroll={{ x: '100%' }}
+          scroll={{ x: 2000 }}
           dataSource={dataSource}
           columns={columns}
           pagination={{
--- src/components/user/tables/TableColumns.tsx
+++ src/components/user/tables/TableColumns.tsx
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 import { getColumnSearchProps } from './TableHelpers';
+import { Badge, Tag } from 'antd';
 
-export const columns = [
+export const columns: any = [
   {
     title: '#',
     key: 'index',
+    width: '3%',
     render: (_: any, __: any, index: any) => <div>{index + 1}</div>,
     shouldCellUpdate: (record: any, prev: any) => !_.isEqual(record, prev),
   },
@@ -20,5 +22,79 @@ export const columns = [
     dataIndex: 'last_name',
     key: 'last_name',
     shouldCellUpdate: (record: any, prev: any) => !_.isEqual(record, prev),
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+    ...getColumnSearchProps('email'),
+    shouldCellUpdate: (record: any, prev: any) => !_.isEqual(record, prev),
+    width: 290,
+  },
+  {
+    title: 'Phone',
+    dataIndex: 'phone',
+    key: 'phone',
+    shouldCellUpdate: (record: any, prev: any) => !_.isEqual(record, prev),
+  },
+  {
+    title: 'Role',
+    dataIndex: 'role',
+    key: 'role',
+    render: (text: any) => (
+      <Tag key={text} color="geekblue">
+        {text}
+      </Tag>
+    ),
+    shouldCellUpdate: (record: any, prev: any) => !_.isEqual(record, prev),
+  },
+  {
+    title: 'Inviter',
+    dataIndex: 'inviter',
+    key: 'inviter',
+    render: (text: any) => (
+      <Tag key={text} color="cyan">
+        {text}
+      </Tag>
+    ),
+    shouldCellUpdate: (record: any, prev: any) => !_.isEqual(record, prev),
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+    render: (status: Boolean) => (
+      <Badge
+        status={status ? 'success' : 'processing'}
+        text={status ? 'Registered' : 'Pending'}
+      />
+    ),
+    shouldCellUpdate: (record: any, prev: any) => !_.isEqual(record, prev),
+  },
+  {
+    title: 'Company',
+    dataIndex: 'company',
+    key: 'company',
+    shouldCellUpdate: (record: any, prev: any) => !_.isEqual(record, prev),
+  },
+  {
+    title: 'Country',
+    dataIndex: 'country',
+    key: 'country',
+    shouldCellUpdate: (record: any, prev: any) => !_.isEqual(record, prev),
+    width: 150,
+  },
+  {
+    title: 'Eir Code',
+    dataIndex: 'zip',
+    key: 'zip',
+    shouldCellUpdate: (record: any, prev: any) => !_.isEqual(record, prev),
+    width: 100,
+  },
+  {
+    title: 'Actions',
+    key: 'actions',
+    fixed: 'right',
+    width: 200,
   },
 ];
