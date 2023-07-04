0f82a2de5bc15f8f59a2884bca612a7eb7a2a6e6
De Jong Yeong <dejong.re2@gmail.com>
2023-07-04 15:47:36 +0100

    add small note on dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,26 @@ import HistoricalData from '@/components/dashboard/HistoricalData';
 import DshLayout from '@/components/dashboard/Layout';
 import { checkUserSessionSsr } from '@/services/auth/check-session-ssr';
 import { getLoginUser } from '@/services/user/query/get-login-user';
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Typography } from 'antd';
 import { GetServerSidePropsContext } from 'next';
 import { ReactNode } from 'react';
 
 // TODO: machine list should populate from database
+
+const { Text } = Typography;
 
 export default function Home() {
   return (
     <main className="w-full h-auto">
       <Breadcrumb items={[{ title: 'Home' }, { title: 'Dashboard' }]} />
       <div className="h-auto mt-7 p-7 bg-white">
+        <div className="mb-8">
+          <Text className="text-gray-500 text-xs">
+            Note: eventually, this page will display near real-time data of the
+            selected machine and other information of interest. Historical data
+            will be in the analytics section
+          </Text>
+        </div>
         <HistoricalData />
       </div>
     </main>
